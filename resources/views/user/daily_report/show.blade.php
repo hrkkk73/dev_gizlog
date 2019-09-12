1ae264d2005c7c5c2f804bbd4b5b5b8f00590418
磯谷浩貴 <>
2019-09-12 19:11:53 +0900

    buttonタグをForm::buttonに置き換え、修正しました。

--- resources/views/user/daily_report/show.blade.php
+++ resources/views/user/daily_report/show.blade.php
@@ -26,7 +26,7 @@
     <a class="btn btn-edit" href="{{ route('report.edit', $dailyReport->id) }}"><i class="fa fa-pencil" aria-hidden="true"></i></a>
     <div class="btn-delete">
       {!! Form::open(['route' => ['report.delete', $dailyReport->id], 'method' => 'DELETE' ]) !!}
-        <button class="btn btn-danger" type="submit"><i class="fa fa-trash-o"></i></button>
+        {!! Form::button('<i class="fa fa-trash-o"></i>', ['type' => 'submit', 'class' => 'btn btn-danger']) !!}
       {!! Form::close() !!}
     </div>
   </div>
