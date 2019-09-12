83c893e79af8092c566893a03de754a2dd95ccad
磯谷浩貴 <>
2019-09-12 15:56:49 +0900

    検索機能のリファクタとPHPDocの@paramの追加＋修正

--- app/Http/Controllers/User/DailyReportController.php
+++ app/Http/Controllers/User/DailyReportController.php
@@ -3,32 +3,32 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\DailyReportRequest;
 use App\Models\DailyReport;
 use Auth;
-use App\Http\Requests\User\DailyReportRequest;
 
 class DailyReportController extends Controller
 {
 
-    private $dailyreport;
+    private $dailyReport;
 
-    public function __construct(DailyReport $instanceClass)
+    public function __construct(DailyReport $dailyReport)
     {
         $this->middleware('auth');
-        $this->dailyreport = $instanceClass;
+        $this->dailyReport = $dailyReport;
     }
 
     /**
      * Display a listing of the resource.
      *
+     * @param App\Http\Requests\User\DailyReportRequest $request
      * @return \Illuminate\Http\Response
      */
     public function index(DailyReportRequest $request)
     {
         $searchMonth = $request->input('search-month');
-        $dailyreports = $this->dailyreport->where('user_id', Auth::id())->where('reporting_time', 'LIKE', "$searchMonth%")->get();
-        // getByUserId(Auth::id());
-        return view('user.daily_report.index', compact('dailyreports'));
+        $dailyReports = $this->dailyReport->getDailyReportList(Auth::id(), $searchMonth);
+        return view('user.daily_report.index', compact('dailyReports'));
     }
 
     /**
@@ -44,15 +44,15 @@ class DailyReportController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Http\Requests\User\DailyReportRequest  $request
      * @return \Illuminate\Http\Response
      */
     public function store(DailyReportRequest $request)
     {
         $input = $request->all();
         $input['user_id'] = Auth::id();
-        $this->dailyreport->fill($input)->save();
-        return redirect()->to('report');
+        $this->dailyReport->fill($input)->save();
+        return redirect()->route('report.index');
     }
 
     /**
@@ -63,8 +63,8 @@ class DailyReportController extends Controller
      */
     public function show($id)
     {
-        $dailyreport = $this->dailyreport->find($id);
-        return view('user.daily_report.show', compact('dailyreport'));
+        $dailyReport = $this->dailyReport->find($id);
+        return view('user.daily_report.show', compact('dailyReport'));
     }
 
     /**
@@ -75,22 +75,22 @@ class DailyReportController extends Controller
      */
     public function edit($id)
     {
-        $dailyreport = $this->dailyreport->find($id);
-        return view('user.daily_report.edit', compact('dailyreport'));
+        $dailyReport = $this->dailyReport->find($id);
+        return view('user.daily_report.edit', compact('dailyReport'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Http\Requests\User\DailyReportRequest  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(DailyReportRequest $request, $id)
     {
-        $dailyreport = $request->all();
-        $this->dailyreport->find($id)->fill($dailyreport)->save();
-        return redirect()->to('report');
+        $inputs = $request->all();
+        $this->dailyReport->find($id)->fill($inputs)->save();
+        return redirect()->route('report.index');
     }
 
     /**
@@ -101,7 +101,7 @@ class DailyReportController extends Controller
      */
     public function destroy($id)
     {
-        $this->dailyreport->find($id)->delete();
-        return redirect()->to('report');
+        $this->dailyReport->find($id)->delete();
+        return redirect()->route('report.index');
     }
 }
